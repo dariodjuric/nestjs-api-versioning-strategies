1b2467ef24b820da6032fbd85ceb7ded8e8957ec
Dario Djuric <dario@thisdot.co>
2022-07-01 15:22:33 +0200

    feat: version extractor using Fastify

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
+import { FastifyRequest } from 'fastify';
+
+const DEFAULT_VERSION = '1';
+
+const extractor = (request: FastifyRequest): string | string[] => {
+  const requestedVersion =
+    <string>request.headers['x-api-version'] ?? DEFAULT_VERSION;
+
+  // If requested version is N, then this generates an array like: ['N', 'N-1', 'N-2', ... , '1']
+  return Array.from(
+    { length: parseInt(requestedVersion) },
+    (_, i) => `${i + 1}`,
+  ).reverse();
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new FastifyAdapter());
   app.setGlobalPrefix('api');
   app.enableVersioning({
-    type: VersioningType.URI,
+    type: VersioningType.CUSTOM,
+    extractor,
+    defaultVersion: DEFAULT_VERSION,
   });
   await app.listen(3000);
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,9 +3,15 @@ import { Controller, Get, Param, Version } from '@nestjs/common';
 @Controller('users')
 export class UsersController {
   @Get()
+  @Version('2')
+  findAll2() {
+    return 'findAll2()';
+  }
+
+  @Get()
   @Version('1')
-  findAll() {
-    return 'findAll()';
+  findAll1() {
+    return 'findAll1()';
   }
 
   @Get(':id')
